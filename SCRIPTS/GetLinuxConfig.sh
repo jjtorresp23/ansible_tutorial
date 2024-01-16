7065e1e2f80a015b9409c8506bd14262040303e9
Juan Torres <juan.torresp@livingston.com>
2024-01-16 12:38:01 -0600

    Updating LR 20230116

--- SCRIPTS/GetLinuxConfig.sh
+++ SCRIPTS/GetLinuxConfig.sh
@@ -1,62 +1,62 @@
-> `hostname -s`_data.out
-echo "########################## HOSTNAME ###########################" >> `hostname -s`_data.out
-uname -a >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################## SERVER DATE ###########################" >> `hostname -s`_data.out
-date >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################### uptime ###########################" >> `hostname -s`_data.out
-uptime  >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################### os ver ###########################" >> `hostname -s`_data.out
-cat /etc/redhat-release >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################### DF -H ###########################" >> `hostname -s`_data.out
-df -h >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################### MOUNT ###########################" >> `hostname -s`_data.out
-mount >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################### LSBLK ###########################" >> `hostname -s`_data.out
-lsblk >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################### BLKID ###########################" >> `hostname -s`_data.out
-blkid >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################### ifconfig ###########################" >> `hostname -s`_data.out
-ifconfig -a >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-ip a >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################### netstat -nr ###########################" >> `hostname -s`_data.out
-netstat -nr >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################### netstat -na ###########################" >> `hostname -s`_data.out
-netstat -na >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################### PS -FEA ###########################" >> `hostname -s`_data.out
-ps -fea >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################### FDISK -L ###########################" >> `hostname -s`_data.out
-fdisk -l >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################### PVS ###########################" >> `hostname -s`_data.out
-pvs >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################### VGS ###########################" >> `hostname -s`_data.out
-vgs >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################### LVS ###########################" >> `hostname -s`_data.out
-lvs >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################### MULTIPATH  ###########################" >> `hostname -s`_data.out
-multipath -ll >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################### oracle ASM list disks ###########################" >> `hostname -s`_data.out
-oracleasm listdisks >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "########################### DMIDECODE ###########################" >> `hostname -s`_data.out
-dmidecode --type=1 >> `hostname -s`_data.out
-echo "   " >> `hostname -s`_data.out
-echo "--------------------    EOF    --------------------" >> `hostname -s`_data.out
+datee=$(date +%F_%H%M%S)
+echo "########################## HOSTNAME ###########################" >> snapshot_`hostname -s`_$datee.out
+uname -a >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################## SERVER DATE ###########################" >> snapshot_`hostname -s`_$datee.out
+date >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################### uptime ###########################" >> snapshot_`hostname -s`_$datee.out
+uptime  >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################### os ver ###########################" >> snapshot_`hostname -s`_$datee.out
+cat /etc/redhat-release >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################### DF -H ###########################" >> snapshot_`hostname -s`_$datee.out
+df -h >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################### MOUNT ###########################" >> snapshot_`hostname -s`_$datee.out
+mount >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################### LSBLK ###########################" >> snapshot_`hostname -s`_$datee.out
+lsblk >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################### BLKID ###########################" >> snapshot_`hostname -s`_$datee.out
+blkid >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################### ifconfig ###########################" >> snapshot_`hostname -s`_$datee.out
+ifconfig -a >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+ip a >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################### netstat -nr ###########################" >> snapshot_`hostname -s`_$datee.out
+netstat -nr >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################### netstat -na ###########################" >> snapshot_`hostname -s`_$datee.out
+netstat -na >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################### PS -FEA ###########################" >> snapshot_`hostname -s`_$datee.out
+ps -fea >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################### FDISK -L ###########################" >> snapshot_`hostname -s`_$datee.out
+fdisk -l >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################### PVS ###########################" >> snapshot_`hostname -s`_$datee.out
+pvs >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################### VGS ###########################" >> snapshot_`hostname -s`_$datee.out
+vgs >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################### LVS ###########################" >> snapshot_`hostname -s`_$datee.out
+lvs >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################### MULTIPATH  ###########################" >> snapshot_`hostname -s`_$datee.out
+multipath -ll >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################### oracle ASM list disks ###########################" >> snapshot_`hostname -s`_$datee.out
+oracleasm listdisks >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "########################### DMIDECODE ###########################" >> snapshot_`hostname -s`_$datee.out
+dmidecode --type=1 >> snapshot_`hostname -s`_$datee.out
+echo "   " >> snapshot_`hostname -s`_$datee.out
+echo "--------------------    EOF    --------------------" >> snapshot_`hostname -s`_$datee.out
 exit
